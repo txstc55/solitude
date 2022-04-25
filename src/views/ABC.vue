c0922eb1109e498a859430fa5c076bdbf49d5c89
Xuan Tang <txstc55@gmail.com>
2022-04-25 22:17:44 +0800

    nvm doesnt work

--- src/views/ABC.vue
+++ src/views/ABC.vue
@@ -101,7 +101,6 @@ export default {
   mounted() {
     // set the color
     this.textbox = document.getElementById("textbox");
-    this.textbox.focus();
     var txt = this.textbox.textContent;
     var newText = "";
     for (var i = 0, l = txt.length; i < l; i++) {
