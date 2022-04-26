e2454701cd7cd1154cfb2cf7cc4aebea33792167
Xuan Tang <txstc55@gmail.com>
2022-04-26 16:36:50 +0800

    back to square 1

--- src/views/ABC.vue
+++ src/views/ABC.vue
@@ -41,11 +41,7 @@ import { GoogleTranslator } from "@translate-tools/core/translators/GoogleTransl
 
 // Use some CORS proxy service address as prefix
 const translator = new GoogleTranslator({
-  headers: {
-    "User-Agent":
-      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36",
-  },
-  //   corsProxy: "https://cors-anywhere.herokuapp.com/https://loneliness.one/",
+  corsProxy: "https://crossorigin.me/",
 });
 
 translator
