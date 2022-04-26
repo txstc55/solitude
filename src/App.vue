1f2268f3d839798193459d994d57eff3ccb59787
Xuan Tang <txstc55@gmail.com>
2022-04-26 15:07:29 +0800

    style change

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@
         transform
         -translate-x-1/2 -translate-y-1/2
         text-white text-xl
-        bg-gray-600/20
+        bg-gray-600/10
         content-center
         pt-20
       "
