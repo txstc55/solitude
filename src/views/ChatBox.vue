8a76ed41798db956b5ac16d25315fb971376486a
Xuan Tang <txstc55@gmail.com>
2022-04-29 23:53:56 +0800

    try on html and body

--- src/views/ChatBox.vue
+++ src/views/ChatBox.vue
@@ -299,4 +299,9 @@ label {
   user-select: none; /* Non-prefixed version, currently
                                   supported by Chrome, Edge, Opera and Firefox */
 }
+
+html,
+body {
+  overscroll-behavior-y: contain;
+}
 </style>
