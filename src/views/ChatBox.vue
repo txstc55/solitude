68b839dcda8c8deb1831ced005eae25fd83de996
Xuan Tang <txstc55@gmail.com>
2022-04-29 23:32:49 +0800

    with text

--- src/views/ChatBox.vue
+++ src/views/ChatBox.vue
@@ -23,15 +23,50 @@
         <div
           v-for="(item, index) in cubeTexts"
           :key="index"
-          class="cube__face noselect"
+          class="
+            cube__face
+            noselect
+            border-2 border-white
+            transform
+            duration-500
+          "
           :style="{
             width: boxWidth + 'px',
             height: boxWidth + 'px',
             'line-height': this.boxWidth + 'px',
             transform: cubeFaceStyles[index],
           }"
+        ></div>
+        <div
+          v-for="(item, index) in cubeTexts"
+          :key="index"
+          class="
+            cube__face
+            noselect
+            border-white
+            transform
+            duration-500
+            break-all font-mono
+          "
+          :class="smallCubeFaceActive[index] ? 'allowScroll' : 'noScroll'"
+          :style="{
+            width: boxWidth + 'px',
+            height: boxWidth + 'px',
+            transform: smallCubeFaceStyles[index],
+            'line-height':
+              (this.smallCubeFaceActive[index]
+                ? this.boxWidth / 12
+                : this.boxWidth) + 'px',
+            'font-size':
+              (this.smallCubeFaceActive[index]
+                ? this.boxWidth / 12
+                : this.boxWidth * 0.8) + 'px',
+            'border-width': (this.smallCubeFaceActive[index] ? 2 : 8) + 'px',
+            'font-weight': (this.smallCubeFaceActive[index] ? '100' : 'bold'),
+          }"
+          @mousedown="this.scaleSmallFace(index)"
         >
-          {{ item }}
+          {{ smallCubeTexts[index] }}
         </div>
       </div>
     </div>
@@ -63,7 +98,10 @@ export default {
       left: null,
       top: null,
       bottom: null,
-      cubeTexts: ["1", "2", "3", "4", "5", "6"],
+      cubeTexts: ["", "", "", "", "", "", ""],
+      smallCubeTexts: ["0", "1", "2", "3", "4", "5"],
+      smallCubeScale: [0.25, 0.25, 0.25, 0.25, 0.25, 0.25],
+      smallCubeFaceActive: [false, false, false, false, false, false],
     };
   },
   methods: {
@@ -124,6 +162,25 @@ export default {
         this.yScreen = newY;
       }
     },
+    scaleSmallFace(index) {
+      if (this.smallCubeScale[index] == 0.25) {
+        this.smallCubeScale[index] = 0;
+        setTimeout(() => {
+          this.smallCubeFaceActive[index] = true;
+          this.smallCubeScale[index] = 0.9;
+          this.smallCubeTexts[index] =
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+          setTimeout(() => {
+            this.smallCubeScale[index] = 0;
+            setTimeout(() => {
+              this.smallCubeFaceActive[index] = false;
+              this.smallCubeTexts[index] = index;
+              this.smallCubeScale[index] = 0.25;
+            }, 800);
+          }, 10000);
+        }, 800);
+      }
+    },
   },
   computed: {
     translateNegativeZStyle() {
@@ -139,23 +196,39 @@ export default {
         "rotateX(-90deg) translateZ(" + this.boxWidth / 2 + "px)",
       ];
     },
-    frontStyle() {
-      return "rotateY(0deg) translateZ(" + this.boxWidth / 2 + "px)";
-    },
-    rightStyle() {
-      return "rotateY(90deg) translateZ(" + this.boxWidth / 2 + "px)";
-    },
-    backStyle() {
-      return "rotateY(180deg) translateZ(" + this.boxWidth / 2 + "px)";
-    },
-    leftStyle() {
-      return "rotateY(-90deg) translateZ(" + this.boxWidth / 2 + "px)";
-    },
-    topStyle() {
-      return "rotateX(90deg) translateZ(" + this.boxWidth / 2 + "px)";
-    },
-    bottomStyle() {
-      return "rotateX(-90deg) translateZ(" + this.boxWidth / 2 + "px)";
+    smallCubeFaceStyles() {
+      return [
+        "rotateY(0deg) translateZ(" +
+          (this.boxWidth / 2 + 0.5) +
+          "px) scale(" +
+          this.smallCubeScale[0] +
+          ")",
+        "rotateY(90deg) translateZ(" +
+          (this.boxWidth / 2 + 0.5) +
+          "px) scale(" +
+          this.smallCubeScale[1] +
+          ")",
+        "rotateY(180deg) translateZ(" +
+          (this.boxWidth / 2 + 0.5) +
+          "px) scale(" +
+          this.smallCubeScale[2] +
+          ")",
+        "rotateY(-90deg) translateZ(" +
+          (this.boxWidth / 2 + 0.5) +
+          "px) scale(" +
+          this.smallCubeScale[3] +
+          ")",
+        "rotateX(90deg) translateZ(" +
+          (this.boxWidth / 2 + 0.5) +
+          "px) scale(" +
+          this.smallCubeScale[4] +
+          ")",
+        "rotateX(-90deg) translateZ(" +
+          (this.boxWidth / 2 + 0.5) +
+          "px) scale(" +
+          this.smallCubeScale[5] +
+          ")",
+      ];
     },
     cubeStyle() {
       return (
@@ -195,8 +268,6 @@ export default {
 
 .cube__face {
   position: absolute;
-  border: 1px solid white;
-  font-weight: bold;
   color: white;
   text-align: center;
   background: rgba(0, 0, 0, 1);
@@ -206,6 +277,15 @@ label {
   margin-right: 10px;
   color: white;
 }
+
+.allowScroll {
+  overflow-y: auto;
+  padding: 10px;
+}
+.noScroll {
+  overflow: hidden;
+}
+
 .noselect {
   -webkit-touch-callout: none; /* iOS Safari */
   -webkit-user-select: none; /* Safari */
