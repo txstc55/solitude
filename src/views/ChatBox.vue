f8a080ba61b5d0b8386e4799edc834d8fd894b53
Xuan Tang <txstc55@gmail.com>
2022-04-30 01:08:09 +0800

    added animation to help with reading

--- src/views/ChatBox.vue
+++ src/views/ChatBox.vue
@@ -52,6 +52,7 @@
             break-all
             font-sans
           "
+          :id="'cube_' + index"
           :class="smallCubeFaceActive[index] ? 'allowScroll' : 'noScroll'"
           :style="{
             width: boxWidth + 'px',
@@ -96,16 +97,11 @@ export default {
         [0, 1, 0],
         [0, 0, 1],
       ],
-      front: null,
-      back: null,
-      right: null,
-      left: null,
-      top: null,
-      bottom: null,
       cubeTexts: ["", "", "", "", "", "", ""],
       smallCubeTexts: ["0", "1", "2", "3", "4", "5"],
       smallCubeScale: [0.25, 0.25, 0.25, 0.25, 0.25, 0.25],
       smallCubeFaceActive: [false, false, false, false, false, false],
+      smallFaces: [null, null, null, null, null, null],
     };
   },
   methods: {
@@ -166,6 +162,31 @@ export default {
         this.yScreen = newY;
       }
     },
+    scrollAnimate(index, direction) {
+      if (this.smallCubeFaceActive[index]) {
+        var lastScrollTop = this.smallFaces[index].scrollTop;
+        if (lastScrollTop <= 0 && direction == -1) {
+          setTimeout(() => {
+            window.requestAnimationFrame(() => {
+              this.scrollAnimate(index, 1);
+            });
+          }, 1500);
+        } else {
+          this.smallFaces[index].scrollTop += direction;
+          if (lastScrollTop == this.smallFaces[index].scrollTop) {
+            setTimeout(() => {
+              window.requestAnimationFrame(() => {
+                this.scrollAnimate(index, -1);
+              });
+            }, 1500);
+          } else {
+            window.requestAnimationFrame(() => {
+              this.scrollAnimate(index, direction);
+            });
+          }
+        }
+      }
+    },
     scaleSmallFace(index) {
       if (this.smallCubeScale[index] == 0.25) {
         this.smallCubeScale[index] = 0;
@@ -175,13 +196,18 @@ export default {
           this.smallCubeTexts[index] =
             "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
           setTimeout(() => {
+            window.requestAnimationFrame(() => {
+              this.scrollAnimate(index, 1);
+            });
+          }, 1500);
+          setTimeout(() => {
             this.smallCubeScale[index] = 0;
             setTimeout(() => {
               this.smallCubeFaceActive[index] = false;
               this.smallCubeTexts[index] = index;
               this.smallCubeScale[index] = 0.25;
             }, 800);
-          }, 10000);
+          }, 15000);
         }, 800);
       }
     },
@@ -203,32 +229,32 @@ export default {
     smallCubeFaceStyles() {
       return [
         "rotateY(0deg) translateZ(" +
-          (this.boxWidth / 2 + 0.5) +
+          (this.boxWidth / 2 + 5) +
           "px) scale(" +
           this.smallCubeScale[0] +
           ")",
         "rotateY(90deg) translateZ(" +
-          (this.boxWidth / 2 + 0.5) +
+          (this.boxWidth / 2 + 5) +
           "px) scale(" +
           this.smallCubeScale[1] +
           ")",
         "rotateY(180deg) translateZ(" +
-          (this.boxWidth / 2 + 0.5) +
+          (this.boxWidth / 2 + 5) +
           "px) scale(" +
           this.smallCubeScale[2] +
           ")",
         "rotateY(-90deg) translateZ(" +
-          (this.boxWidth / 2 + 0.5) +
+          (this.boxWidth / 2 + 5) +
           "px) scale(" +
           this.smallCubeScale[3] +
           ")",
         "rotateX(90deg) translateZ(" +
-          (this.boxWidth / 2 + 0.5) +
+          (this.boxWidth / 2 + 5) +
           "px) scale(" +
           this.smallCubeScale[4] +
           ")",
         "rotateX(-90deg) translateZ(" +
-          (this.boxWidth / 2 + 0.5) +
+          (this.boxWidth / 2 + 5) +
           "px) scale(" +
           this.smallCubeScale[5] +
           ")",
@@ -246,7 +272,11 @@ export default {
       );
     },
   },
-  mounted() {},
+  mounted() {
+    for (var i = 0; i < 6; i++) {
+      this.smallFaces[i] = document.getElementById("cube_" + i);
+    }
+  },
   created() {
     this.boxWidth = Math.max(
       200,
